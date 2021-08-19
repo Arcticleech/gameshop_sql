8c882e5d1fce0f7ab194488f953816366234c9e1
Sean <s.j.oconnor@hotmail.co.uk>
2021-08-19 14:53:21 +0100

    third update

--- gamestop_schema.sql
+++ gamestop_schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE games (
 	game_id INT AUTO_INCREMENT,
 	game_title VARCHAR(60) NOT NULL,
 	game_genre VARCHAR(40) NOT NULL,
-	game_price DECIMAL NOT NULL,
+	game_price DECIMAL(100,2) NOT NULL,
 	game_details VARCHAR(100) NOT NULL,
 	PRIMARY KEY (game_id)
 );
@@ -16,7 +16,7 @@ CREATE TABLE games (
 CREATE TABLE customers(
 	customer_id INT AUTO_INCREMENT,
 	customer_name VARCHAR(60) NOT NULL,
-	customer_age TINYINT NOT NULL,
+	customer_age INT NOT NULL,
 	customer_phone_number VARCHAR(15) NOT NULL,
 	customer_email VARCHAR(60) NOT NULL,
 	PRIMARY KEY (customer_id)
@@ -34,8 +34,8 @@ CREATE TABLE orders(
 	order_id INT AUTO_INCREMENT,
 	fk_game_id INT,
 	fk_customer_id INT,
-	age_verify BOOLEAN NOT NULL
-	date_of_purchase DATETIME NOT NULL,
+	age_verify BOOLEAN NOT NULL,
+	date_of_purchase DATE NOT NULL,
 	fk_staff_id INT,
 	PRIMARY KEY (order_id),
 	FOREIGN KEY (fk_game_id) REFERENCES games(game_id),
