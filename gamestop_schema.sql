10e8de7c55d89631f5f1bf83313970e547353ccc
Arcticleech <87663799+Arcticleech@users.noreply.github.com>
2021-08-19 16:22:37 +0100

    Update gamestop_schema.sql

--- gamestop_schema.sql
+++ gamestop_schema.sql
@@ -12,11 +12,6 @@ CREATE TABLE games (
 	PRIMARY KEY (game_id),
 );
 
-CREATE TABLE price(
-	game_price DECIMAL(10,2) NOT NULL,
-	PRIMARY KEY (game_price)
-);
-
 CREATE TABLE customers(
 	customer_id INT AUTO_INCREMENT,
 	customer_name VARCHAR(60) NOT NULL,
@@ -38,12 +33,10 @@ CREATE TABLE orders(
 	order_id INT AUTO_INCREMENT,
 	fk_game_id INT,
 	fk_customer_id INT,
-	fk_game_price, DECIMAL(10,2),
 	date_of_purchase DATE NOT NULL,
 	fk_staff_id INT,
 	PRIMARY KEY (order_id),
 	FOREIGN KEY (fk_game_id) REFERENCES games(game_id),
 	FOREIGN KEY (fk_customer_id) REFERENCES customers(customer_id),
-	FOREIGN KEY (fk_staff_id) REFERENCES staff(staff_id),
-	FOREIGN KEY (fk_game_price) REFERENCES price(game_price)
+	FOREIGN KEY (fk_staff_id) REFERENCES staff(staff_id)
 );
